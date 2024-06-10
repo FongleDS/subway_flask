1823450668ab397baa1d22dd72b833c04002b451
leaKoh <pinksy0311@gmail.com>
2024-06-10 19:45:09 +0900

    .

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import io
 from gtts import gTTS
 from playsound import playsound
 import pygame
+from google.cloud import dialogflow
+import uuid
 
 
 app = Flask(__name__)
@@ -72,6 +74,30 @@ def text_to_speech():
         return jsonify({"message": "Text has been converted to speech and played"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route('/send-message', methods=['POST'])
+def send_message():
+    # Dialogflow 세션 클라이언트 설정
+    session_client = dialogflow.SessionsClient()
+
+    # 요청에서 메시지 추출
+    message = request.json['message']
+
+    # Dialogflow 세션 ID와 프로젝트 ID 설정
+    project_id = 'kioskdialouge'
+    # UUID를 생성하여 세션 ID로 사용
+    session_id = str(uuid.uuid4())
+    session = session_client.session_path(project_id, session_id)
+
+    # 텍스트 입력 설정
+    text_input = dialogflow.TextInput(text=message, language_code="ko-KR")
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    # Dialogflow에 요청 보내기
+    response = session_client.detect_intent(session=session, query_input=query_input)
+    response_text = response.query_result.fulfillment_text
+
+    return jsonify({'reply': response_text})
 
 if __name__ == '__main__':
     app.run(debug=True)
